c5fb671af70718d94eceb1f25cf0e5ae90a66505
Bhargava K K <bhargavakk13@gmail.com>
2025-08-22 19:19:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const age = parseInt(document.querySelector("#age").value);
-const gender = document.querySelector("input[name='gender']:checked");
-const weight = parseFloat(document.querySelector("#weight").value);
-const height = parseFloat(document.querySelector("#height").value);
-const activity = parseInt(document.querySelector("#activity").value);
+// const age = parseInt(document.querySelector("#age").value);
+// const gender = document.querySelector("input[name='gender']:checked");
+// const weight = parseFloat(document.querySelector("#weight").value);
+// const height = parseFloat(document.querySelector("#height").value);
+// const activity = parseInt(document.querySelector("#activity").value);
 const form = document.querySelector("#form");
 let h2 = document.querySelector("#water-intake");
 
 let waterIntake = function(b){
     b = b/1000.0
-    h2.textContent=`You need to consume ${b.toFixed(0)} L of water daily`;
+    h2.textContent=`You need to consume ${b.toFixed(2)} L of water daily`;
 }
 
 form.addEventListener('submit', function(evt){
     evt.preventDefault();
-
+    const formdata = new FormData(form);
+    const age = parseInt(formdata.get('age'));
+    const gender = formdata.get('gender');
+    const weight = parseFloat(formdata.get('weight'));
+    const height = parseFloat(formdata.get('height'));
+    const activity = parseInt(formdata.get('activity'));
     if (isNaN(age) || isNaN(weight) || isNaN(height) || !gender || isNaN(activity) || activity === 0) {
         alert("Please fill out all fields with valid information.");
         return;
     }
-    
     let bmr = 0;
     bmr = (10 * weight) + (6.25 * height) - (5 * age);
 
-    if (gender.value === "male") {
+    if (gender === "male") {
         bmr += 5;
     } else {
         bmr -= 161;
@@ -50,7 +54,7 @@ form.addEventListener('submit', function(evt){
     }
 
     waterIntake(bmr);
-});
+})
 
 function clearFormOnLoad(){
     if(form){
